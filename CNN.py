598c7b4ed359bfa33c4371161d59e81e0996615c
Mathias <mathias.opland@bym.oslo.kommune.no>
2019-04-17 02:03:21 +0200

    Added rotation of images

--- CNN.py
+++ CNN.py
@@ -1,57 +1,107 @@
 import numpy as np
 
 from keras import Model
-from keras.layers import Input, Conv2D, Flatten, Dense, Dropout, MaxPooling2D, add, ReLU
-from keras.callbacks import EarlyStopping
+from keras.layers import Input, Conv2D, Flatten, Dense, Dropout, MaxPooling2D, add, ReLU, BatchNormalization, \
+    concatenate
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import load_model
+from skimage import feature
+from sklearn.model_selection import train_test_split
+
 
 
 class CNN:
     def __init__(self):
-        self.input_shape = (20, 20, 3)
+        self.input_shape = (20, 20, 1)
         self.model = self.network()
 
     def network(self):
         input_img = Input(shape=self.input_shape)
+        input_edges = Input(shape=self.input_shape)
 
-        def residual_learning(input_layer):
-            y = Conv2D(64, kernel_size=(1, 1), strides=(1, 1))(input_layer)
+        def residual_layers(input_layer):
+            y = Conv2D(64, kernel_size=(3, 3), strides=(1, 1), padding='same')(input_layer)
+            y = BatchNormalization()(y)
             y = ReLU()(y)
-            y = Conv2D(64, kernel_size=(1, 1), strides=(1, 1))(y)
+            y = Conv2D(64, kernel_size=(3, 3), strides=(1, 1), padding='same')(y)
+            y = BatchNormalization()(y)
             y = ReLU()(y)
             y = add([y, input_layer])
             y = ReLU()(y)
             return y
 
-        x = Conv2D(64, kernel_size=(3, 3))(input_img)
-        x = ReLU()(x)
-        x = MaxPooling2D(pool_size=(2, 2))(x)
-        x = residual_learning(x)
-        x = residual_learning(x)
-        x = Flatten()(x)
+        def branches(input_layer):
+            y = Conv2D(64, kernel_size=(3, 3), strides=(1, 1))(input_layer)
+            y = ReLU()(y)
+            # y = Conv2D(64, kernel_size=(3, 3), strides=(2, 2))(y)
+            # y = ReLU()(y)
+            y = MaxPooling2D(pool_size=(2, 2))(y)
+            y = residual_layers(y)
+            # y = residual_layers(y)
+            # y = residual_layers(y)
+            y = MaxPooling2D(pool_size=(2, 2))(y)
+            y = BatchNormalization()(y)
+            y = ReLU()(y)
+            return Flatten()(y)
+
+        img = branches(input_img)
+        edg = branches(input_edges)
+
+        x = concatenate([img, edg])
+        x = BatchNormalization()(x)
         x = Dense(128, activation='relu')(x)
         x = Dropout(0.5)(x)
         x = Dense(64, activation='relu')(x)
         x = Dropout(0.5)(x)
         x = Dense(26, activation='softmax')(x)
 
-        model = Model(inputs=input_img, outputs=x)
+        model = Model(inputs=[input_img, input_edges], outputs=x)
         model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
         model.summary()
         return model
 
-    def train(self, x, y):
+    def train(self, img, edges, y):
         early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=5, verbose=0, mode='auto')
-        callbacks = [early_stopping]
-        self.model.fit(x=x, y=y, epochs=50, batch_size=64, validation_split=0.1, verbose=2, callbacks=callbacks)
-        history = self.model.evaluate(x=x, y=y)
+        mcp_save = ModelCheckpoint('model_weights.hdf5', save_best_only=True, monitor='val_loss', mode='min')
+        callbacks = [early_stopping, mcp_save]
+        self.model.fit(x=[img, edges], y=y, epochs=50, batch_size=64, validation_split=0.1, verbose=2,
+                       callbacks=callbacks)
+
+        model = load_model('model_weights.hdf5')
+        history = model.evaluate(x=[img, edges], y=y)
         print("Train loss: ", history[0], ", train accuracy: ", history[1])
 
-    def test(self, x, y):
-        history = self.model.evaluate(x=x, y=y)
+    def train_generator(self, img, edges, y):
+        data_generator = ImageDataGenerator(featurewise_center=True,
+                                            featurewise_std_normalization=True,
+                                            rotation_range=90,
+                                            width_shift_range=5,
+                                            height_shift_range=5,
+                                            horizontal_flip=True,
+                                            zca_whitening=True)
+        early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=5, verbose=0, mode='auto')
+        mcp_save = ModelCheckpoint('model_smooth_weights.hdf5', save_best_only=True, monitor='val_loss', mode='min')
+        callbacks = [early_stopping, mcp_save]
+        img_train, img_val, y_train, y_val = train_test_split(img, y, test_size=0.1, random_state=42)
+        edges_train, edges_val = train_test_split(edges, test_size=0.1, random_state=42)
+        data_generator.fit(img_train)
+        self.model.fit_generator(data_generator.flow([img_train, edges_train], y_train, batch_size=32),
+                                 steps_per_epoch=len(img) / 32, epochs=100, callbacks=callbacks,
+                                 validation_data=([img_val, edges_val], y_val), verbose=2)
+        model = load_model('model_smooth_weights.hdf5')
+        history = model.evaluate(x=[img, edges], y=y)
+        print("Train loss: ", history[0], ", train accuracy: ", history[1])
+
+    def test(self, img, edges, y):
+        model = load_model('model_smooth_weights.hdf5')
+        history = model.evaluate(x=[img, edges], y=y)
         print("Test loss: ", history[0], ", test accuracy: ", history[1])
+        return history[1]
 
 
-def run(x_train, x_test, y_train, y_test):
+def run(img_train, img_test, edges_train, edges_test, y_train, y_test):
     network = CNN()
-    network.train(x_train, y_train)
-    network.test(x_test, y_test)
+    network.train(img_train, edges_train, y_train)
+    test_accuracy = network.test(img_test, edges_test, y_test)
+    return test_accuracy
--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-import pandas as pd
 import numpy as np
-from pathlib import Path
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import normalize, MinMaxScaler
+from scipy.ndimage import rotate
 import cv2
 import os
+import random
 
 import CNN
+from data_processing import DataProcessing
+
+random.seed(42)
+
+dir = 'dataset\chars74k-lite'
 
 
-def load_images_from_folder(dir):
+def load_dataset(dir):
     images = np.zeros([7112, 20, 20, 3])
     targets = np.zeros([7112, 26])
     chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
@@ -28,21 +32,36 @@ def load_images_from_folder(dir):
     return images, targets
 
 
-def load_dataset():
-    dir = 'dataset\chars74k-lite'
-    chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    x, y = load_images_from_folder(dir)
-    return x, y
+def rotate_training_data(img, edg, y):
+    new_img = np.zeros([len(img)*4, len(img[0]), len(img[0, 0]), len(img[0, 0, 0])])
+    new_edg = np.zeros([len(img) * 4, len(img[0]), len(img[0, 0]), len(img[0, 0, 0])])
+    new_y = np.zeros([len(y)*4, len(y[0])])
+    new_img[:len(img)] = img
+    new_edg[:len(img)] = img
+    new_y[:len(y)] = y
+    for i in range(3):
+        new_img[len(img)*(i+1):len(img)*(i+2)] = rotate(input=img, angle=90*(i+1), axes=(1, 2))
+        new_edg[len(edg)*(i+1):len(edg)*(i+2)] = rotate(input=edg, angle=90*(i+1), axes=(1, 2))
+        new_y[len(y)*(i+1):len(y)*(i+2)] = y
+    return new_img, new_edg, new_y
 
 
-def main():
-    x, y = load_dataset()
-    x = x/255
-    print(x)
-    x_train, x_test, y_train, y_test = train_test_split(rescaledX, y, test_size=0.2, random_state=42)
-    CNN.run(x_train, x_test, y_train, y_test)
+def main(dir):
+    print('Loading data...')
+    x, y = load_dataset(dir)
+    print('Preprocessing data..')
+    data = DataProcessing(x, y)
+    average_num = 10
+    test_accuracy = np.zeros(average_num)
+    for i in range(average_num):
+        print('Training network ', i+1)
+        random_state = 42+i
+        img_train, img_test, y_train, y_test = train_test_split(data.x, data.y, test_size=0.2, random_state=random_state)
+        edges_train, edges_test = train_test_split(data.edges, test_size=0.2, random_state=random_state)
+        # img_train, edges_train, y_train = rotate_training_data(img_train, edges_train, y_train)
+        test_accuracy[i] = CNN.run(img_train, img_test, edges_train, edges_test, y_train, y_test)
+    print('Average test accuracy over ', average_num, ' trials: ', np.mean(test_accuracy))
 
 
 if __name__ == "__main__":
-    main()
+    main(dir)
