e3fef381eb2d2baeb918c84b6cc4ddf6b6809f7f
sn00tdogg <adriantb@stud.ntnu.no>
2019-05-03 09:27:20 +0200

    Update character_detection.py

--- character_detection.py
+++ character_detection.py
@@ -8,12 +8,12 @@ import numpy as np
 from models import CNN
 from load_data import load_data_detection, load_data_chars
 
-random.seed(100)
+random.seed(100)                  # Sets seed to get the same results across runs       
 
 model_weights = 'model_weights.hdf5'
 
 
-def data_processing(x):
+def data_processing(x):           # Grayscale, normalization
     x = x.astype('float32')
     x /= 255  # Make between 0 and 1
     x = rgb2gray(x)  # Convert to grey scale
@@ -21,7 +21,7 @@ def data_processing(x):
     return x
 
 
-def check_if_all_zero(img):
+def check_if_all_zero(img):       # Checks if sliding window is all white/all black
     img = img.reshape(400)
     percentage_zero = 0
     for i in range(len(img)):
@@ -35,31 +35,32 @@ def check_if_all_zero(img):
         return False
 
 
-def slicing_window(img, model_weights):
+def slicing_window(img, model_weights):     # Implements sliding windows with constant width/height
     stride_width = 5
     stride_height = 5
     width = 20
     height = 20
+    #Import model weights from fitted CNN characterisation task
     network = CNN.CNN(num_classes=27, sample=img[0, 0:0 + height, 0:0 + height], model_weights=model_weights,
                       network_type='simple')
 
     predictions = []
-    for i in range(0, img.shape[1]-height, stride_height):
+    for i in range(0, img.shape[1]-height, stride_height):  #Slides windows across image, saves letters to prediction list
         for j in range(0, img.shape[2]-width, stride_width):
             predicted_prob = network.predict_character(img[:, i:i + height, j:j + width])
             if np.amax(predicted_prob) > 0.7 and np.argmax(predicted_prob) != 0 and check_if_all_zero(img[:, i:i + height, j:j + width]):
                 predictions.append([np.argmax(predicted_prob), j, i])
                 print(str(chr(96+np.argmax(predicted_prob))), j, i)
-    fig, ax = plt.subplots(1)
-    ax.imshow(img[0, :, :, 0], cmap='gray')
-    for i in range(len(predictions)):
+    fig, ax = plt.subplots(1)   #Make figure
+    ax.imshow(img[0, :, :, 0], cmap='gray') # Show image
+    for i in range(len(predictions)):       # Add red frames onto letter predictions
         rect = patches.Rectangle((predictions[i][1], predictions[i][2]), 20, 20, linewidth=1, edgecolor='r', facecolor='none')
         # Add the patch to the Axes
         ax.add_patch(rect)
     plt.show()
 
 
-def character_detection(model_weights):
+def character_detection(model_weights):     # Perform detection task on both test images
     img1, img2 = load_data_detection()
     img1 = data_processing(img1)
     img2 = data_processing(img2)
