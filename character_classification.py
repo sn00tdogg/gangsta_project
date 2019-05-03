3eeda556a87f91ed9a559523e6307b1d27dafea1
sn00tdogg <adriantb@stud.ntnu.no>
2019-05-03 09:03:16 +0200

    Update character_classification.py

--- character_classification.py
+++ character_classification.py
@@ -7,14 +7,14 @@ import plots
 random.seed(100)
 
 
-def character_classification():
+def character_classification():             #Loads and shapes data
     print('Loading data...')
     x, y = load_data_chars()
     print('Processing data..')
     print('Training data shape: ', x.shape)
     print('Test data shape: ', y.shape)
-    plots.plot_filters(x[0])
-    SVM.svm(x, y)
+    plots.plot_filters(x[0])                # Plots single filters
+    SVM.svm(x, y)                           # Fits all 4 models
     Naive_Bayes.naive_bayes(x, y)
     KNN.knn(x, y)
     CNN.fit_cnn(x, y, trials=1, network_type='simple')
